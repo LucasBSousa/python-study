90cc27b6ebfadfd2023fba1462dd556fed973f4e
LUCAS BERNARDO DE SOUSA <0030481721014@lti.fatecso.edu.br>
2017-10-04 11:02:39 -0300

    exercício 3 da minimaratona

--- minimaratona3.py
+++ minimaratona3.py
@@ -1,4 +1,6 @@
-def estacionamento(a,me,d,h,mi):
+# UNSOLVEDs
+
+def valor(a,me,d,h,mi):
     v = 0
 
     if a > 0:
@@ -12,6 +14,35 @@ def estacionamento(a,me,d,h,mi):
     if mi > 0:
         v += mi*5
 
-    print(v)
+    return v
 
-estacionamento(1,1,1,1,1)
+def periodo(a1, me1, d1, h1, mi1, a2, me2, d2, h2, mi2):
+    minutos = 0
+    anos1 = 0
+    meses1 = 0
+    dias1 = 0
+    horas1 = 0
+    anos2 = 0
+    meses2 = 0
+    dias2 = 0
+    horas2 = 0
+
+    if a1 > 0:
+        anos1 = a1*15
+    if me1 > 0:
+        meses1 = me1*25
+    if d1 > 0:
+        dias1 = d1*20
+    if h1 > 0:
+        horas1 = h1*50
+    minutos1 = mi1
+
+    minutos = minutos2 - minutos1
+    horas = minutos/50
+    dias = horas/20
+    meses = dias/25
+    anos = meses/15
+
+    print(minutos1, horas1, dias1, meses1, anos1)
+
+periodo(1,0,0,0,0, 2,0,0,0,0)
